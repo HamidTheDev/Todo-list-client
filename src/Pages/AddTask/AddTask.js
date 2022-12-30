a4106d1beb63e162ad5e486165656fc14b6eb8e7
Hamid Chowdhury <100404131+HamidTheDev@users.noreply.github.com>
2022-12-30 23:33:31 +0600

    uploaded

--- src/Pages/AddTask/AddTask.js
+++ src/Pages/AddTask/AddTask.js
@@ -1,6 +1,6 @@
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import React, { useContext } from 'react';
-import  { useState } from "react";
+import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import { AuthContext } from '../../AuthProvider/AuthProvider';
 import { storage } from '../../Firebase/firebase.config';
@@ -8,18 +8,18 @@ import { storage } from '../../Firebase/firebase.config';
 
 const fileTypes = ["JPG", "PNG", "GIF"];
 const AddTask = () => {
-    const {user}=useContext(AuthContext)
-    const [urls,setUrls]= useState('')
+    const { user } = useContext(AuthContext)
+    const [urls, setUrls] = useState('')
 
 
     const handleChange = (file) => {
-      const imageRef = ref(storage, `images/${file.name}`);
-        uploadBytes(imageRef,file).then(res=>getDownloadURL(res.ref).then(url=>setUrls(url)))
-      console.log(user)
+        const imageRef = ref(storage, `images/${file.name}`);
+        uploadBytes(imageRef, file).then(res => getDownloadURL(res.ref).then(url => setUrls(url)))
+        console.log(user)
     };
 
     console.log(urls)
-    const addToDo=(e)=>{
+    const addToDo = (e) => {
 
         e.preventDefault()
 
@@ -27,41 +27,41 @@ const AddTask = () => {
 
 
         const form = e.target;
-        const inputValue=form.add.value;
-        const toDOInput={
-            todo:inputValue,
-            email:user?.email,
-            img:urls
+        const inputValue = form.add.value;
+        const toDOInput = {
+            todo: inputValue,
+            email: user?.email,
+            img: urls
 
 
         }
         console.log(toDOInput)
-        fetch('http://localhost:5000/addtodo',{
-            method:'POST',
-            headers:{
+        fetch('https://todo-list-server.onrender.com/addtodo', {
+            method: 'POST',
+            headers: {
                 'content-type': 'application/json'
             },
             body: JSON.stringify(toDOInput)
         })
-        .then(res=>res.json())
-        .then(data=>{
-            if(data.acknowledged){
-                alert('To Do added successfully')
-                form.reset()
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    alert('To Do added successfully')
+                    form.reset()
+                }
+            })
 
     }
 
 
     return (
         <div className='flex flex-col justify-center items-center my-12 '>
-            <FileUploader  handleChange={handleChange} name="file" types={fileTypes} />
+            <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
 
-           <form onSubmit={addToDo} className='w-full flex justify-center items-center my-4'>
-           <input name='add' className='border-[#333] text-center w-1/3 bg-[#e4ddf4] p-3 rounded-lg shadow-md ' placeholder='add todo' type="text" />
+            <form onSubmit={addToDo} className='w-full flex justify-center items-center my-4'>
+                <input name='add' className='border-[#333] text-center w-1/3 bg-[#e4ddf4] p-3 rounded-lg shadow-md ' placeholder='add todo' type="text" />
                 <button type='submit' className='border-none px-2'><i class="fa-solid fa-circle-plus text-[#0c92bf] text-4xl"></i> </button>
-           </form>
+            </form>
         </div>
     );
 };
--- src/Pages/MyTask/MyTask.js
+++ src/Pages/MyTask/MyTask.js
@@ -9,19 +9,19 @@ const MyTask = () => {
   const [value, setValue] = useState('')
   console.log(value);
   useEffect(() => {
-    fetch(`http://localhost:5000/addmytask?email=${user?.email}`)
+    fetch(`https://todo-list-server.onrender.com/addmytask?email=${user?.email}`)
       .then(res => res.json())
       .then(data => setGetToDo(data))
   }, [user?.email])
 
   const handleComplete = (id) => {
-    fetch(`http://localhost:5000/complete/${id}`, {
+    fetch(`https://todo-list-server.onrender.com/complete/${id}`, {
       method: 'PUT',
 
     })
       .then(res => res.json())
       .then(data => {
-        fetch(`http://localhost:5000/addmytask?email=${user?.email}`)
+        fetch(`https://todo-list-server.onrender.com/addmytask?email=${user?.email}`)
           .then(res => res.json())
           .then(data => setGetToDo(data))
       })
@@ -29,13 +29,13 @@ const MyTask = () => {
   }
   const handleDelete = () => {
 
-    fetch(`http://localhost:5000/addmytask/${id}`, {
+    fetch(`https://todo-list-server.onrender.com/addmytask/${id}`, {
       method: 'DELETE'
 
     })
       .then(res => res.json())
       .then(data => {
-        fetch(`http://localhost:5000/addmytask?email=${user?.email}`)
+        fetch(`https://todo-list-server.onrender.com/addmytask?email=${user?.email}`)
           .then(res => res.json())
           .then(data => setGetToDo(data))
         alert("deleted")
@@ -50,19 +50,19 @@ const MyTask = () => {
 
   const handleUpdate = (todoId) => {
     const textInput = {
-      todo:value
+      todo: value
     }
 
-    fetch(`http://localhost:5000/updated/${todoId}`, {
+    fetch(`https://todo-list-server.onrender.com/updated/${todoId}`, {
       method: 'PUT',
       headers: {
-        'content-type':'application/json'
+        'content-type': 'application/json'
       },
-      body:JSON.stringify(textInput)
+      body: JSON.stringify(textInput)
     })
       .then(res => res.json())
       .then(data => {
-        fetch(`http://localhost:5000/addmytask?email=${user?.email}`)
+        fetch(`https://todo-list-server.onrender.com/addmytask?email=${user?.email}`)
           .then(res => res.json())
           .then(data => setGetToDo(data))
         alert("Todo Updated!")
@@ -79,10 +79,10 @@ const MyTask = () => {
         return !todo.completed && <div className='my-4 lg:text-lg text-sm font-thin lg:font-semibold bg-slate-100 block p-5 rounded shadow-md relative'>
           <div className='w-[90%] flex gap-10'>
             <img className='w-9 lg:w-20 rounded' src={todo.img} alt="" />
-            <textarea id={todo._id} onChange={()=>setValue(document.getElementById(todo._id).value)} onFocus={handleEdit} className='border p-3 w-full bg-slate-100 outline-none' type="text" defaultValue={todo.todo} />
+            <textarea id={todo._id} onChange={() => setValue(document.getElementById(todo._id).value)} onFocus={handleEdit} className='border p-3 w-full bg-slate-100 outline-none' type="text" defaultValue={todo.todo} />
             {
               edit &&
-                <button onClick={()=>handleUpdate(todo._id)} className='h-7 absolute right-0 bottom-2 lg:static px-4 mx-3 text-sm text-white rounded bg-[#0c92bf]'>Update</button>
+              <button onClick={() => handleUpdate(todo._id)} className='h-7 absolute right-0 bottom-2 lg:static px-4 mx-3 text-sm text-white rounded bg-[#0c92bf]'>Update</button>
             }
           </div>
           <i onClick={() => handleComplete(todo._id)} class="cursor-pointer fa-regular fa-square-check absolute lg:right-14 right-5 text-2xl top-7 mr-4"></i>
